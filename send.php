c6ccf34c2130ede7b75b8a01ba0ace4960a596dc
Luis Enrique Sánchez de Anda <39448740+LuisEnrique2912@users.noreply.github.com>
2020-05-20 16:41:34 -0500

    Update send.php

--- send.php
+++ send.php
@@ -7,11 +7,8 @@
     $mensaje = $_POST['message'];
     
     $header = "Enviado desde la página oficial de Grupo TMM";
-    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-    $header .= "Mime-Version: 1.0 \r\n";
-    $header .= "Content-Type: text/plain";
     
-    $mensajeCompleto .= $mensaje . "\nAtentamente: " . $mail;
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $mail;
     
     mail($destinatario, $asunto, $mensajeCompleto, $header);
     
